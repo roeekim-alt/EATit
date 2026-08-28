81bb9f6829c8bced521958520ec05d71d81a7130
roeekim-alt <roeekim@gmail.com>
2026-08-28 21:24:57 +0300

    Add React entry point

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import EatIt from './App.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <EatIt />
+  </React.StrictMode>,
+)
